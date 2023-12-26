6c50806fe1d766cd35a9529962daf56e75078c9b
Kristjan Pekk <kristjan.pekk@gmail.com>
2023-12-27 01:42:11 +0200

    now adding subtitles ot videos

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from openai import OpenAI
 from ideaGenerator import generate_ideas
 from imageCreator import generate_image
 from imageToVideo import generate_video_from_image
+from addSubtitlesToVideo import add_subtitles_to_video
 from voiceGenerator import generate_commentary_audio
+
+#todo generate voice of length n, make sure video and voice are almost as long
 
 count = 2
 client = OpenAI(api_key=get_openai_api_key())
@@ -36,14 +39,14 @@ with open('output/list_of_ideas.txt', 'r') as file:
                                     1, 0.5, 0.5,
                                     1.2, 0.5, 0.5)
 
-        break
         # generate voicover
         generate_commentary_audio(client, commentary, output_directory)
 
         # add voiceover to video
 
-        # add subtitles???????
-
+        # add subtitles
+        add_subtitles_to_video(output_directory, "initial_video.mp4", "video_with_subtitles.mp4", 
+                               "commentary.mp3", "subtitles.srt")
 
 
         # upload video to yt (title, hastags?)
