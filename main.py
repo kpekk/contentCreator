cc7df86fbd5346d0748fabc9e0d2beded5458b92
Kristjan Pekk <kristjan.pekk@gmail.com>
2023-12-30 00:14:22 +0200

    match the generated video length to the audio's length

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, math
 from common import get_openai_client
 from ideaGenerator import generate_ideas
 from imageCreator import generate_image
@@ -6,8 +6,8 @@ from videoGenerator import generate_video_from_image
 from subtitleManager import add_subtitles_to_video
 from voiceGenerator import generate_commentary_audio
 from voiceoverToVideo import add_audio_to_video
+from moviepy.editor import AudioFileClip
 
-# TODO generate voice of length n, make sure video and voice are almost as long
 # todo make video, commentary etc names parameters not hardcoded into smaller files
 # todo delay for audio and subtitles, about 0.2, 0.3
 # TODO text file into folder with title, description? and tags
@@ -18,8 +18,9 @@ def clean_everything_but_final_video(working_directory, file_to_keep="final_vide
             file_path = os.path.join(working_directory, file)
             os.remove(file_path)
 
-count = 2
+count = 5
 client = get_openai_client()
+video_frame_rate = 25 # frames per second
 
 # generate prompts
 generate_ideas(client, count)
@@ -42,14 +43,17 @@ with open('output/list_of_ideas.txt', 'r') as file:
         # generate the base image
         generate_image(client, image_description, output_directory, 'original_image')
 
-        # image to video todo
-        generate_video_from_image(output_directory,
-                                    200, 25,
-                                    1, 0.5, 0.5,
-                                    1.2, 0.5, 0.5)
-
         # generate voicover
         generate_commentary_audio(client, commentary, output_directory)
+
+        # calculate the duration of the video to math the length of the audio
+        audio_duration = AudioFileClip(f"{output_directory}/commentary.mp3").duration
+        video_duration_in_frames = (int) (math.ceil(audio_duration * video_frame_rate * 100) / 100)
+        
+        # generate video
+        generate_video_from_image(output_directory, video_duration_in_frames, video_frame_rate,
+                                    1, 0.5, 0.5,
+                                    1.2, 0.5, 0.5)
 
         # add voiceover to video
         add_audio_to_video(f"{output_directory}/initial_video.mp4", f"{output_directory}/commentary.mp3",
@@ -60,5 +64,4 @@ with open('output/list_of_ideas.txt', 'r') as file:
                                "commentary.mp3", "subtitles.srt")
 
         clean_everything_but_final_video(output_directory)
-        break
         # upload video to yt (title, hastags?)
